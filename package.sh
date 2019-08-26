a59dc983185f0141ba947fab363dafe3997b8e57
Matthieu Corageoud <matthieu.corageoud@gmail.com>
2019-08-26 14:55:39 +0200

    Add node modules in package

--- package.sh
+++ package.sh
@@ -6,4 +6,10 @@ npm install --production
 rm -f SHA256SUMS
 sha256sum package.json *.js LICENSE > SHA256SUMS
 find node_modules -type f -exec sha256sum {} \; >> SHA256SUMS
-npm pack
+
+TARFILE=$(npm pack)
+tar xzf ${TARFILE}
+cp -r node_modules ./package
+tar czf ${TARFILE} package
+rm -rf package
+echo "Created ${TARFILE}"
